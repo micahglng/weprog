88cd3e28b9d62e47f720bbadee6529a6e9d2137f
micahglng <gmicah397@gmail.com>
2026-07-15 12:22:02 +0800

    Add files via upload

--- process.php
+++ process.php
@@ -1,24 +1,29 @@
 <?php
+if($_SERVER["REQUEST_METHOD"]== "POST"){
 $name = $_POST["fullname"];
 $age = $_POST["age"];
-$gender = $_POST["gender"];
-$course = $_POST["course"];
-$message = $_POST["message"];
+            $gender = $_POST["gender"];
+            $course = $_POST["course"];
+            $message = $_POST["message"];
 
-echo"<h1>Student Registration Form</h1>";
+            echo"<h1>Student Registration Form</h1>";
 
-echo "Name:$name<br>";
-echo "Age:$age<br>";
-echo "Gender:$gender<br>";
-echo "Course:$course<br>";
-echo "Message:$message<br>";
+            echo "Name:$name<br>";
+            echo "Age:$age<br>";
+            echo "Gender:$gender<br>";
+            echo "Course:$course<br>";
+            echo "Message:$message<br>";
 
-echo"<br>";
+            echo"<br>";
 
-echo"Hobbies:<br>";
-foreach($_POST["hobbies"]as $hobby){
-    echo $hobby."<br>";
+            echo"Hobbies:<br>";
+            foreach($_POST["hobbies"]as $hobby){
+            echo $hobby."<br>";
+}
 }
 
+else{
+    echo"GHAHUHD*WD";
+}
 
 ?>
